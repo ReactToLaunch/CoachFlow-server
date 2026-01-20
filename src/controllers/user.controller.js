f243f94dc29d3e47a38b4a7f56c2ede2edd87be2
Syed Aasim <aasimsyed398@gmail.com>
2026-01-20 14:48:31 +0530

    Done linting and reset password

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -68,10 +68,6 @@ if (!createdUser) {
   throw new ApiError(500, "Somthing went wrong while regestering User")
 }
 
-// Immediately reset isVerified for forgot password flow
-user.isVerified = false;
-await user.save({ validateBeforeSave: false });
-
 return res.json(
     new ApiResponse(200, {user: createdUser},  "User Registered Successfully",)
 
@@ -120,9 +116,7 @@ const loginUser = asyncHandler( async (req, res) => {
         secure: true
     }
 
-  // reset the OTP/verification flag
-  user.isVerified = false; 
-  await user.save({ validateBeforeSave: false });
+ 
 
 
   // sending response
@@ -140,66 +134,8 @@ const loginUser = asyncHandler( async (req, res) => {
 ))
 
 })
-// ---------------------------------------------------------------------
-
-// creating forgot password controller
-
-// User clicks on forgot password 
-// user sent to another page
-// user enters email
-
-const forgot_password = asyncHandler( async (req, res) => {
-
-  console.log("REQ HEADERS FULL:", req.headers);
-
-  console.log("REQ HEADERS:", req.headers["content-type"]);
-  console.log("REQ BODY:", req.body);
-
-  const { email } = req.body || {};
-
-    // ***E:  check if email field is provided***
-
-    if (email?.trim() === "") 
-    {throw new ApiError(401, "all Fields are required")}
-
-    // ***E: check if email is correct format***
-
-    if (!email || typeof email !== "string") {
-      throw new ApiError(400, "Email must be a string");
-    }
 
 
-
-    let atCount = 0;
-    for (let char of email){
-      if (char === "@") {
-        atCount++;
-        if (atCount > 1){
-          throw new ApiError(400, "multiple @ characters found in email")
-        }
-      }
-      
-    }
-    if (atCount === 0) {
-      throw new ApiError(400, "invalid email format, missing @ character")
-    }
-
-  // ***E: check if email exists***
-  const userExists = await User.exists({ email });
-  
-  if (!userExists) {
-    throw new ApiError(404, "No user found with this email");
-  }
-
-
-  return res.status(200).json(
-    new ApiResponse(
-      200,
-      { email },
-      "Email verified. Proceed to OTP"
-    )
-  );
-})
 
 // user clicks on send otp
 // do otp verification
@@ -209,12 +145,12 @@ const forgot_password = asyncHandler( async (req, res) => {
 // now the user can reset password
 // send user to another window 
 
-const reset_password = asyncHandler( async (req, res) => {
+const resetPassword = asyncHandler( async (req, res) => {
   // get the new password from req.body
-  const { email, newPassword } = req.body;
+  const { email, newPassword, Otp } = req.body;
 
 // E: check if email and new password fields are provided
-  if ([email, newPassword].some((field) => field?.trim() === "")) {
+  if ([email, newPassword, Otp].some((field) => field?.trim() === "")) {
     throw new ApiError(401, "all Fields are required")
   }
 // E: check if new password is 8 characters long
@@ -228,6 +164,25 @@ const reset_password = asyncHandler( async (req, res) => {
   if (!user) {
     throw new ApiError(404, "User not found");
   }
+   
+   const record = await Otp.findOne({ email });
+
+  if (!record) {
+    throw new ApiError(404, "OTP not found or expired");
+  }
+
+
+  if (record.expiresAt < Date.now()) {
+    await Otp.deleteOne({_id: record._id})
+    throw new ApiError(400, "Otp is Expired ")
+  }
+
+  const isValid = await record.validateOtp(Otp);
+
+  if (!isValid) {
+    throw new ApiError(404, "Invalid OTP");
+  }
+ 
 
   // 🔑 THIS IS ALL YOU DO
   user.password = newPassword;
@@ -236,9 +191,7 @@ const reset_password = asyncHandler( async (req, res) => {
   await user.save();
 
 
-  //  changing isVerified to false for security reasons
-  user.isVerified = false;
-  await user.save({ validateBeforeSave: false });
+ 
     
   // send response
   return res.status(200).json(
@@ -247,10 +200,7 @@ const reset_password = asyncHandler( async (req, res) => {
       "Password reset successful. Please login again."
     )
   );
-// send user back to login page
 
-
-// allow user to reset password
 });
 
 
@@ -266,7 +216,11 @@ const Otpgenerate = asyncHandler(async (req, res) => {
     if (!email) {
         throw new ApiError(400, "Email field is required");
     }
+      const existingUser = await User.findOne({ email });
 
+    if (!existingUser) {
+        throw new ApiError(404, "User with this email does not exist");
+    }
     
     const generatedOtp = await generateOtp();
     
@@ -369,5 +323,5 @@ const getAllStudents = asyncHandler( async (req, res) => {
 
 
 
-export {registerUser, verifyOtp, Otpgenerate, loginUser, getAllStudents, forgot_password,reset_password};
+export {registerUser, verifyOtp, Otpgenerate, loginUser, getAllStudents,resetPassword};
 
