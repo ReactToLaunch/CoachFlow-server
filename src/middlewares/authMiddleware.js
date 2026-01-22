e8d5b03d7c19e1e67ff3492f271872eb739fbcb0
Syed Aasim <aasimsyed398@gmail.com>
2026-01-22 19:15:23 +0530

    added middleWare to protect Api

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,32 +2,30 @@ import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 import { Admin } from "../models/admin.model.js"; 
 
-// -------------------------------------------------------------------------
-// 1. PROTECT STUDENT ROUTES (The "Bouncer" for the App)
-// -------------------------------------------------------------------------
+
 export const protect = async (req, res, next) => {
   let token;
 
-  // Check if header exists and starts with "Bearer"
+
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
     try {
-      // Get the token string (Remove "Bearer ")
+      
       token = req.headers.authorization.split(" ")[1];
 
-      // Decode the token using STUDENT SECRET
+      
       const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-      // Find the user in DB (excluding password)
+      
       req.user = await User.findById(decoded._id).select("-password");
 
       if (!req.user) {
         return res.status(401).json({ message: "Not authorized, user not found" });
       }
 
-      next(); // Move to the Controller
+      next(); 
     } catch (error) {
       console.error(error);
       res.status(401).json({ message: "Not authorized, token failed" });
@@ -39,9 +37,7 @@ export const protect = async (req, res, next) => {
   }
 };
 
-// -------------------------------------------------------------------------
-// 2. PROTECT ADMIN ROUTES (The "Bouncer" for the Dashboard)
-// -------------------------------------------------------------------------
+
 export const protectAdmin = async (req, res, next) => {
   let token;
 
@@ -52,11 +48,10 @@ export const protectAdmin = async (req, res, next) => {
     try {
       token = req.headers.authorization.split(" ")[1];
 
-      // Decode using ADMIN SECRET (Different key!)
+      
       const decoded = jwt.verify(token, process.env.ADMIN_ACCESS_TOKEN_SECRET);
 
-      // We can either fetch from DB (Secure) or use the token payload (Fast)
-      // Since you are using the DB method for Admin earlier:
+      
       req.admin = await Admin.findById(decoded._id).select("-password");
 
       if (!req.admin) {
